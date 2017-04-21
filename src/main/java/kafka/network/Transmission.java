a809bafea75a8c9e931ea1361fa350a2babc8522
zlfkyo@sina.com <zlfkyo@sina.com>
2017-04-21 18:13:39 +0800

    Transmission

--- src/main/java/kafka/network/Transmission.java
+++ src/main/java/kafka/network/Transmission.java
@@ -1,21 +1,67 @@
 package kafka.network;
 
+import kafka.common.KafkaException;
+import kafka.utils.Logging;
+
+import java.nio.ByteBuffer;
+import java.nio.channels.GatheringByteChannel;
+import java.nio.channels.ReadableByteChannel;
+
 /**
- * Created by Administrator on 2017/4/20.
+ * Represents a stateful transfer of data to or from the network
  */
-public class Transmission extends Logging {
+public abstract class Transmission extends Logging {
 
-        def complete: Boolean
+    public abstract boolean complete();
 
-        protected def expectIncomplete(): Unit = {
-        if(complete)
-            throw new KafkaException("This operation cannot be completed on a complete request.")
+    protected void expectIncomplete() {
+        if (complete())
+            throw new KafkaException("This operation cannot be completed on a complete request.");
     }
 
-    protected def expectComplete(): Unit = {
-        if(!complete)
-            throw new KafkaException("This operation cannot be completed on an incomplete request.")
+    protected void expectComplete() {
+        if (!complete())
+            throw new KafkaException("This operation cannot be completed on an incomplete request.");
+    }
+}
+
+
+/**
+ * A transmission that is being received from a channel
+ */
+abstract class Receive extends Transmission {
+
+    public ByteBuffer buffer;
+
+    public abstract Integer readFrom(ReadableByteChannel channel);
+
+    public Integer readCompletely(ReadableByteChannel channel) {
+        Integer totalRead = 0;
+        while (!complete()) {
+            Integer read = readFrom(channel);
+            trace(read + " bytes read.");
+            totalRead += read;
+        }
+        return totalRead;
     }
 
 }
+
+/**
+ * A transmission that is being sent out to the channel
+ */
+abstract class Send extends Transmission {
+
+    public abstract Integer writeTo(GatheringByteChannel channel);
+
+    public Integer  writeCompletely(GatheringByteChannel channel) {
+        Integer totalWritten = 0;
+        while (!complete()) {
+            Integer written = writeTo(channel);
+            trace(written + " bytes written.");
+            totalWritten += written;
+        }
+        return totalWritten;
+    }
+
 }
