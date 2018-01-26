4d3e56be28c40ee8952d44d8351c80ac8cc17ca0
zlfkyo@sina.com <zlfkyo@sina.com>
2018-01-26 18:21:12 +0800

    增加 equals信息

--- src/main/java/kafka/api/LeaderAndIsr.java
+++ src/main/java/kafka/api/LeaderAndIsr.java
@@ -28,6 +28,29 @@ public class LeaderAndIsr {
         this(leader, LeaderAndIsr.initialLeaderEpoch, isr, LeaderAndIsr.initialZKVersion);
     }
 
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        LeaderAndIsr that = (LeaderAndIsr) o;
+
+        if (leader != null ? !leader.equals(that.leader) : that.leader != null) return false;
+        if (leaderEpoch != null ? !leaderEpoch.equals(that.leaderEpoch) : that.leaderEpoch != null) return false;
+        if (isr != null ? !isr.equals(that.isr) : that.isr != null) return false;
+        return zkVersion != null ? zkVersion.equals(that.zkVersion) : that.zkVersion == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = leader != null ? leader.hashCode() : 0;
+        result = 31 * result + (leaderEpoch != null ? leaderEpoch.hashCode() : 0);
+        result = 31 * result + (isr != null ? isr.hashCode() : 0);
+        result = 31 * result + (zkVersion != null ? zkVersion.hashCode() : 0);
+        return result;
+    }
+
     @Override
     public String toString() {
         return JSON.toJSONString(ImmutableMap.of("leader", leader, "leader_epoch", leaderEpoch, "isr", isr));
--- src/main/java/kafka/producer/PartitionAndLeader.java
+++ src/main/java/kafka/producer/PartitionAndLeader.java
@@ -15,4 +15,24 @@ public class PartitionAndLeader {
         this.partitionId = partitionId;
         this.leaderBrokerIdOpt = leaderBrokerIdOpt;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PartitionAndLeader that = (PartitionAndLeader) o;
+
+        if (topic != null ? !topic.equals(that.topic) : that.topic != null) return false;
+        if (partitionId != null ? !partitionId.equals(that.partitionId) : that.partitionId != null) return false;
+        return leaderBrokerIdOpt != null ? leaderBrokerIdOpt.equals(that.leaderBrokerIdOpt) : that.leaderBrokerIdOpt == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = topic != null ? topic.hashCode() : 0;
+        result = 31 * result + (partitionId != null ? partitionId.hashCode() : 0);
+        result = 31 * result + (leaderBrokerIdOpt != null ? leaderBrokerIdOpt.hashCode() : 0);
+        return result;
+    }
 }
